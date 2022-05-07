c4c06ed8a80446f1a7315a8831c193b3de7dba09
Pronunts <44618066+pronunts@users.noreply.github.com>
2022-05-06 21:14:18 -0400

    Se agrego el modalinfo

--- src/ModalInfo/index.js
+++ src/ModalInfo/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './ModalInfo.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./ModalInfo.css";
 
-function ModalInfo({children}){
-    return ReactDOM.createPortal(
-        <div className='backgroundModal'>
-            { children }
-        </div>,
-        document.getElementById('modal_info')
-    )
+function ModalInfo({ children }) {
+  return ReactDOM.createPortal(
+    <div className="backgroundModal">{children}</div>,
+    document.getElementById("modal_info"),
+  );
 }
 
-export {ModalInfo}
+export { ModalInfo };
